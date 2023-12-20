bfe8fa3c2545c40bffce643f707306dbef4ffc72
Natalia <nle1208icloud.com@iMac-van-Natalia.local>
2023-12-20 18:37:32 +0100

    My first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.WriteLine("Hello world!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 1.5;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
