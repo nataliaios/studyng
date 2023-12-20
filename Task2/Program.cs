f47d178f53e91830ef6ced9d93da1f7e79f10876
Natalia <nle1208icloud.com@iMac-van-Natalia.local>
2023-12-21 00:24:23 +0100

    lesson1:task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i = 5;
+int j = 6;
+int sum = i + j;
+Console.WriteLine(sum);
+
+long l = 6;
+long sum2 = l + i;
+Console.WriteLine(sum2);
+
+double d = 3.5;
+double sum3 = d + i;
+Console.WriteLine(sum3);
+
+int product1 = i * j;
+long product2 = i * l;
+double product3 = i * d;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
