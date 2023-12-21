5ab318da565e57b7e5f95217d1eb2329a691130a
Natalia <nle1208icloud.com@iMac-van-Natalia.local>
2023-12-21 01:14:22 +0100

    lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 50;
+int b = 34;
+int c = 12;
+int d = 35;
+int e = 8;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
