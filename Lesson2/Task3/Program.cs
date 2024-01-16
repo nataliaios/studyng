ab0639d597083aebf6beda3d9c88d57cf3631006
Natalia <nle1208icloud.com@iMac-van-Natalia.local>
2024-01-16 22:10:38 +0100

    add Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 4, 3, 22, 41, 6, 7, 13, 11, 8 };
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 2 == 0) 
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
